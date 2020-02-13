9084d84852ee2eb3f7788c0d39b3b0ac03962374
kanoop640 <kanoop640@gmail.com>
2020-02-13 14:33:28 +0530

    [Anoop]AddedSnakeLadderGameLayout

--- SanekLadder.sh
+++ SanekLadder.sh
@@ -1,2 +1,33 @@
 echo "            Welcome to Snake and Ladder Game "
+count=100
+declare -A arr[100]
+print()
+{
+	for(( i=0;i<10;i++ ))
+	{
+		if(( $i%2!=0 ))
+		then
+		count=$(( count-10 ))
+		fi
+		for(( j=0; j<10; j++ ))
+		{
+			if(( $i%2==0 ))
+			then
+			arr[$j]=$count
+			count=$(( count-1 ))
+			echo -ne " "${arr[$j]}
+			else
+			count=$(( count+1 ))
+			arr[$j]=$count
+			echo -ne " "${arr[$j]}
+			fi
+		}
+		echo
+		if(( $i%2!=0 ))
+		then
+			count=$(( count-10 ))
+		fi
+	}
+}
+print
 
